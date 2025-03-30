19e03bbf66875e627e5c2eeeabb46ba0cb890d60
paral1el7 <1062150697@qq.com>
2025-03-31 01:49:12 +1030

    Completed Step 7: handle cache miss, request from origin server, write to cache and respond to client. Tested with curl.

--- Proxy.py
+++ Proxy.py
@@ -140,6 +140,7 @@ while True:
       address = socket.gethostbyname(hostname)
       # Connect to the origin server
       # ~~~~ INSERT CODE ~~~~
+      originServerSocket.connect((address, 80))
       # ~~~~ END CODE INSERT ~~~~
       print ('Connected to origin Server')
 
